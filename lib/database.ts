7445950baa22df954194c135e48986cea8541641
WalterPython34 <morellosteve80@yahoo.com>
2025-07-18 21:59:00 -0500

    Update database.ts

--- lib/database.ts
+++ lib/database.ts
@@ -1,10 +1,13 @@
+import { Pool, neonConfig } from "@neondatabase/serverless"
 import { drizzle } from "drizzle-orm/neon-serverless"
-import { Pool } from "@neondatabase/serverless"
+import ws from "ws"
 import * as schema from "./schema"
 
+neonConfig.webSocketConstructor = ws
+
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set")
+  throw new Error("DATABASE_URL must be set. Did you forget to provision a database?")
 }
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-export const db = drizzle(pool, { schema })
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+export const db = drizzle({ client: pool, schema })
