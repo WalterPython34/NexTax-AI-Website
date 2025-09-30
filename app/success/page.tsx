556232829750c07eff72ca86783db42244401e5f
WalterPython34 <morellosteve80@yahoo.com>
2025-09-30 14:06:27 -0500

    Update page.tsx

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -354,6 +354,12 @@ export default function SuccessPage() {
               You can now complete our simple EIN questionnaire and we'll handle the rest. Your business will be legally
               formed and ready to operate within 48 hours.
             </p>
+             <Link href="/startsmart">
+              <Button size="lg" className="bg-white text-emerald-600 hover:bg-slate-100 px-8 py-4 text-lg">
+                View Questionnaire
+                <ArrowRight className="ml-2 w-5 h-5" />
+              </Button>
+            </Link>
         </div>
         </div>
       </section>
