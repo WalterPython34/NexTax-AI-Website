f5a76ab3eaacdad21c0a5ac9f13ac22d9b456838
WalterPython34 <morellosteve80@yahoo.com>
2025-10-22 17:43:44 -0500

    Update page.tsx

--- app/test-demo/page.tsx
+++ app/test-demo/page.tsx
@@ -201,7 +201,7 @@ export default function TestDemoPage() {
             </div>
 
              {/* Video/Image Right */}
-            <div className="relative justify-center">
+            <div className="relative flex justify-center">
               <video
                 src="/videos/tax_expert.mp4"
                 autoPlay
