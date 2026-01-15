f99edb3b0bb9b6ddabb89d4bc899c6acaf6351ee
WalterPython34 <morellosteve80@yahoo.com>
2026-01-15 10:48:32 -0500

    Update page.tsx

--- app/ecomm/page.tsx
+++ app/ecomm/page.tsx
@@ -174,8 +174,10 @@ export default function TestLandingPage() {
           {/* 60/40 Grid Layout */}
           <div className="flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-16">
             {/* Left Column - Quiz Card (60%) */}
+            <div className="lg:col-span-3">
             <div className="flex-shrink-0">
               <TaxSavingsWizard variant="card" />
+              </div>
               </div>
             
             {/* Right Column - Creator Image (40%) */}
