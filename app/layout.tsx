7e4f703b4b84a2134fc7300735e589a65bce4401
WalterPython34 <morellosteve80@yahoo.com>
2026-01-15 15:04:19 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,7 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-5VHGTSGCCN');
+            gtag('config', 'AW-17878724249');
           `}
         </Script>
       </body>
