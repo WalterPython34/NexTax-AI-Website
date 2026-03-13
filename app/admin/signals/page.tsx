8964abb0b824feb8b58a24c7c4e312716079f37d
WalterPython34 <morellosteve80@yahoo.com>
2026-03-13 15:04:42 -0400

    Create UploadSignalsPage component for signal uploads

---
+++ app/admin/signals/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import SignalUploader from "@/components/signal-uploader";
+
+export default function UploadSignalsPage() {
+  return <SignalUploader />;
+}
